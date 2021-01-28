48ea0fcb2bf7df38c805fccf28f9d03e6caad20d
Devon Wolfkiel <devonwolf@tutanota.com>
2021-01-28 14:25:50 -0800

    Clean up files

--- quest/quest.js
+++ quest/quest.js
@@ -1,32 +1,11 @@
-import quests from '../data/data.js';
-import questData from '../data/data.js';
-import { findByID } from '../utils/data-utils.js';
 import { renderQuest, selectChoice, renderChoiceOutcome } from '../utils/quest-render-utils.js';
 
 renderQuest();
 const choiceForm = document.querySelector('form');
-// const params = new URLSearchParams(window.location.search);
-// const questObject = findByID(questID, questData);
 
 choiceForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    
     const choiceID = selectChoice(choiceForm);
     renderChoiceOutcome(choiceID);
-  // questObject.choices find by ID to get choice
-        // window.location = `./?id=${questID}&choice=${choiceID}`;
 });
-
-
-// try/catch block didn't turn out as useful as I thought
-
-// catch (error) {
-//     const params = new URLSearchParams(window.location.search);
-//     // const questID = params.get('id');
-//     const choiceID = params.get('choice');
-//     console.log(choiceID);
-  
-    
-// 	// const choiceObject = findByID(choiceID, questData.choices);
-// 	// console.log(choiceObject);
-// }
-
--- utils/choice-render-utils.js
+++
@@ -1,8 +0,0 @@
-import questData from '../data/data.js';
-import { findByID } from '../utils/data-utils.js';
-
-const params = new URLSearchParams(window.location.search);
-const choiceID = params.get('choices');
-
-const choiceObject = findByID(choiceID, questData.choices);
-console.log(choiceObject);
