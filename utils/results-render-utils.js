442d930562cdda61f3f4ede16a3f79de050248e3
Devon Wolfkiel <devonwolf@tutanota.com>
2021-01-28 17:38:22 -0800

    Improve results page styling

--- utils/results-render-utils.js
+++ utils/results-render-utils.js
@@ -3,12 +3,14 @@ import { getUser } from '../utils/data-utils.js';
 export function renderQuestSummary() {
     const h2 = document.querySelector('h2');
     const p = document.querySelector('p');
+    const pEval = document.getElementById('user-eval');
 
     const user = getUser();
     const capitalClass = user.charClass.charAt(0).toUpperCase() + user.charClass.slice(1);
 
     h2.textContent = `${user.name} the ${capitalClass}`;
-    p.textContent = `You end your adventures with ${user.gold} gold and ${user.hp} hp.\n${evaluateUser(user)}`;
+    p.textContent = `You end your adventures with ${user.gold} gold and ${user.hp} hp.`;
+    pEval.textContent = evaluateUser(user);
 }
 
 function evaluateUser(user) {
